1bb17fd5803088a0cad07542bb91020c705f50fd
deliy <371054926@qq.com>
2015-12-10 15:54:49 +0800

    testmodel
    test－－－－

---
+++ LeftMenuDemo/TestModel.h
@@ -0,0 +1,13 @@
+//
+//  TestModel.h
+//  LeftMenuDemo
+//
+//  Created by swhl on 15/12/10.
+//  Copyright © 2015年 lxj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestModel : NSObject
+
+@end
---
+++ LeftMenuDemo/TestModel.m
@@ -0,0 +1,13 @@
+//
+//  TestModel.m
+//  LeftMenuDemo
+//
+//  Created by swhl on 15/12/10.
+//  Copyright © 2015年 lxj. All rights reserved.
+//
+
+#import "TestModel.h"
+
+@implementation TestModel
+
+@end
